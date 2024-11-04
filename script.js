20285e1e096f2f17f0415c8ffbaa2771b8285c79
shiva954 <orsushiva138@gmail.com>
2024-11-04 21:07:09 +0530

    my meassage

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("this is a log method");
+console.error("this is a error method ");
+console.warn("this is a warning method");
+console.info("this is a info method");
+console.debug("this is a debug method");
+console.log(console);
